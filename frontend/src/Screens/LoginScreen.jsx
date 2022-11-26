3e5465b358d6693142cabd2d74ac9548bd815760
Maaz Khan <maazkhan7769@gmail.com>
2022-11-26 22:55:57 +0530

    added responsiveness

--- frontend/src/Screens/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen.jsx
@@ -1,18 +1,38 @@
 import styled from 'styled-components'
+import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai'
+import { useState } from 'react'
 
 const MainContainer = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
+    @media (max-width: 768px) {
+      flex-direction: column-reverse;
+    }
 `
 
-const Container = styled.div`
+const Container1 = styled.div`
   width:${props => props.width};
-  height: 100vhpx;
+  height: 100vh;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  @media (max-width: 768px) {
+   display: none;
+  }
+`
+const Container2 = styled.div`
+  width:${props => props.width};
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
 `
 const Image = styled.img`
 width: 380.22px;
@@ -29,6 +49,11 @@ align-items: center;
 box-shadow: 0px 5px 15px 0px  rgba(0, 0, 0, 0.244) ;
 border-radius: 10px;
 margin-right: 30px;
+@media (max-width: 768px) {
+  width: 100%;
+  height: 100%;
+  margin-right: 0px;
+}
 `
 
 const Heading = styled.h1`
@@ -65,12 +90,25 @@ const InputBox = styled.input`
 const InputCheckBox = styled.input`
   color: #737B86;
 `
+
+const InputPwDiv = styled.div`
+position: relative;
+margin-top:-10px
+`
+const Span = styled.span`
+  z-index: 9999;
+	position: absolute;
+	top: 45%;
+	right: 0;
+`
+
 const Link = styled.a`
   font-size: 0.8rem;
   font-weight: ${props => props.weight || 400};
   line-height: ${props=> props.lineHeight || 0};
   text-decoration: ${props => props.decoration || "none"};
   color: #F78719;
+  cursor: pointer;
 `
 
 const Wrapper1 = styled.div`
@@ -90,29 +128,53 @@ const PwDiv = styled.div`
   position: absolute;
   right: 0px;
 `
-const Button = styled.button`
+const LoginButton = styled.button`
   background-color: #1575A7;
   border: 1px solid #1575A7;
   width: 60%;
   height: 2.5rem;
   border-radius: 5px;
   color: white;
+  cursor: pointer;
+`
+
+const EyeButton = styled.div`
+background: transparent;
+cursor: pointer;
 `
 
 const LoginScreen = () => {
+  const [type, SetType] = useState("password")
+
+  const clickHandler = () => {
+    if (type === 'password'){
+      SetType('text')
+    } else {
+      SetType('password')
+    }
+  }
   return (
     <MainContainer>
-      <Container width="40%">
+      <Container1 width="40%">
         <Image src='/Assets/leftimg.png'/>
-      </Container>
-      <Container width="60%">
+      </Container1>
+      <Container2 width="60%">
         <FormContainer>
           <Heading>Login</Heading>
           <Form>
           <Label>Login ID</Label>
           <InputBox type='text' placeholder='Enter Login ID'/>
+          <InputPwDiv>
           <Label>Password</Label>
-          <InputBox type='password' placeholder='Enter Password'/>
+          <InputBox type={type} placeholder='Enter Password'/>
+          <Span>
+            <EyeButton onClick={clickHandler}>
+            {type === 'password' ?
+                <AiFillEyeInvisible size='1.5rem' color='#737B86'/> 
+                : <AiFillEye size='1.5rem' color='#737B86'/>}
+            </EyeButton>
+          </Span>
+          </InputPwDiv>   
           <Wrapper1 mt='-10px'>
           <InputCheckBox type='checkbox' />
           <Label weight='400' color="#737B86" >Remember Me</Label>
@@ -126,7 +188,7 @@ const LoginScreen = () => {
           <Link decoration='underline'>Terms & Conditions</Link>
           </Wrapper1>
           <Wrapper2>
-          <Button>Login</Button>
+          <LoginButton>Login</LoginButton>
           <div>
           <Label weight='400'>Don’t have an account? </Label>
           <Link decoration='underline' weight='700'>Register Here</Link>
@@ -134,7 +196,7 @@ const LoginScreen = () => {
           </Wrapper2>
           </Form>
         </FormContainer>
-      </Container>
+      </Container2>
     </MainContainer>
   )
 }
